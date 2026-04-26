f8c3b843629ac534dbc9a2acd82f5f9e7ec183d5
charrafimed <mhch5003@gmail.com>
2026-04-26 10:26:00 +0200

    fix accrodion docs

--- components/accordion/files/content.blade.php
+++ components/accordion/files/content.blade.php
@@ -1,4 +1,4 @@
-<div style="display: none" x-show="isVisible" x-collapse class="">
+<div style="display: none" x-show="isVisible" x-collapse {{ $attributes }}>
     <div {{ $attributes->merge(['class' => 'px-6 pb-4 pt-2']) }}>
         {{ $slot }}
     </div>
